2ed889b16cb3ed0b0ce1425a201d91cb5c0c1dae
tapanxd <tapan007panchal@gmail.com>
2025-02-10 22:49:43 -0400

    updated backend URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://todo-app-backend-gpwl.onrender.com/",
 });
 
 export default API;
